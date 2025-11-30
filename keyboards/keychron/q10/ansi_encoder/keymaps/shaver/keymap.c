dcb6252f7504e0a978198f5fddb9985c966f0a7c
Mike Shaver <shaver@off.net>
2025-11-30 16:16:28 -0500

    home row mods (GASC)

--- keyboards/keychron/q10/ansi_encoder/keymaps/shaver/keymap.c
+++ keyboards/keychron/q10/ansi_encoder/keymaps/shaver/keymap.c
@@ -37,6 +37,18 @@ enum my_keycodes {
 #define KC_FLXP LGUI(KC_E)
 #define LAY_PAD MO(WIN_NUMPAD)
 
+/* left hand home-row mods: A (GUI)    S (ALT)    D (SHIFT)   F (CTRL) */
+#define GUI_A   LGUI_T(KC_A)
+#define ALT_S   LALT_T(KC_S)
+#define SFT_D   LSFT_T(KC_D)
+#define CTL_F   LCTL_T(KC_F)
+
+/* right hand home-row mods: J (CTRL)    K (SHIFT)   L (ALT)    ; (GUI) */
+#define CTL_J   RCTL_T(KC_J)
+#define SFT_K   RSFT_T(KC_K)
+#define ALT_L   LALT_T(KC_L)  /* note: LALT here because RALT is used as compose */
+#define GUI_SC  RGUI_T(KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_89(
         KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_INS,             KC_DEL,
@@ -58,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
         LAY_PAD,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        _______,  KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_END,
+        _______,  KC_ESC,   GUI_A,    ALT_S,    SFT_D,    CTL_F,    KC_G,      KC_H,     CTL_J,    SFT_K,    ALT_L,    GUI_SC,   KC_QUOT,            KC_ENT,             KC_END,
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,  MO(WIN_FN),                     KC_SPC,             KC_RALT,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
 
