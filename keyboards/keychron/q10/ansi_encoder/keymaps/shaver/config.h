25d6acdeb410002f0e53ec45b055f2c5869b78a9
Mike Shaver <shaver@off.net>
2025-12-02 09:11:12 -0500

    tune some h-r-m timings, not sure they're quite right yet

--- keyboards/keychron/q10/ansi_encoder/keymaps/shaver/config.h
+++ keyboards/keychron/q10/ansi_encoder/keymaps/shaver/config.h
@@ -1,2 +1,4 @@
-#define PERMISSIVE_HOLD
-
+/*** home-row mods ***/
+#define TAPPING_TERM 160 /* time before a tap becomes a hold, default 200, milliseconds */
+#define PERMISSIVE_HOLD  /* second press before TAPPING_TERM is hold */
+#define QUICK_TAP_TERM 0 /* disable double-press auto repeat */
